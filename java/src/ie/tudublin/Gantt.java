f688c06c73489059642de1b09beb125461b49902
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2021-04-17 20:45:09 +0100

    Adding display Tasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -9,6 +9,8 @@ import processing.data.TableRow;
 public class Gantt extends PApplet
 {	
 	ArrayList<Task> tasks = new ArrayList<Task>();
+	public int leftMargin = 50;
+	public int rightMargin = 10;
 	
 	public void settings()
 	{
@@ -35,6 +37,27 @@ public class Gantt extends PApplet
     //         text(i, border / 2, y);
     //     }
     // }
+
+	void drawGrid()
+    {
+        float border = 0.1f * width;
+        textAlign(CENTER, CENTER);
+        //float drawable = width - (border * 2.0f);
+        //float gap = drawable / 10.0f;
+        for(int i = -5 ; i <=5 ; i ++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            float y = map(i, -5, 5, border, height - border);
+            //float x = border + ((i + 5) * gap);
+            //float y = border + ((i + 5) * gap);
+            // stroke(0, 0, 255);
+            // line(x, border,x, height - border);
+            // line(border, y, width - border, y);
+            // fill(255);
+            // text(i, x, border / 2);
+            // text(i, border / 2, y);
+        }
+    }
 
 	public void loadTasks()
 	{
